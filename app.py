db3a52c606dd90014fbbf27fe43353c2a68ae857
= <=>
2019-07-18 14:35:59 -0400

    add main function to run app, add bagels endpoint

--- app.py
+++ app.py
@@ -12,5 +12,14 @@ def hello():
     return f'Hello, {escape(name)}!'
 
 
+@app.route('/bagels')
+def bagels():
+    name = request.args.get("name", "Fred")
+    greeting = "{} likes bagels.".format(name)
+    return greeting
+
+
 if __name__ == "__main__":
     print("Hello, world!")
+
+    app.run('localhost', '5000')
